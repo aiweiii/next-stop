888ffa8c481caf7e061f21b39bd6bfa74427cf71
tehxueer29 <tehxueer29@gmail.com>
2021-11-04 15:18:00 +0800

    database related

--- validation_page/validation_page.php
+++ validation_page/validation_page.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,11 +11,12 @@
     <link rel="stylesheet" href="validation_page.css">
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </head>
+
 <body>
     <nav>
         <div class="nav-logo">
             <a href="../landing_page/landing_page.html" id="nav-logo">next stop.</a>
-        </div>  
+        </div>
 
         <div class="nav-items" id="nav-items">
             <a href="../homepage/homepage.php" class="nav-item" id="nav-home">Home</a>
@@ -43,21 +45,70 @@
                 </div>
             </form>
         </div>
-        <a href="../homepage/homepage.php" class="confirm-btn">Confirm</a>
+        <a href="../homepage/homepage.php" class="confirm-btn" onclick="addUni()">Confirm</a>
     </div>
 
     <script>
         var navItems = document.getElementById("nav-items");
         navItems.style.maxHeight = "0px";
-        function toggleMenu(){
-            if( navItems.style.maxHeight == "0px" ){
+
+        function toggleMenu() {
+            if (navItems.style.maxHeight == "0px") {
                 navItems.style.maxHeight = "200px";
             } else {
                 navItems.style.maxHeight = "0px";
             }
         }
+
+        function addUni() {
+            <?php
+            require_once 'common.php';
+            $email = $_SESSION['email'];
+            echo $email;
+
+            // Create the DAO object to facilitate connection to the database.
+            $dao = new UserDAO();
+            $user = $dao->get($email);
+            // Check if the email exists
+            if ($user != null) {
+                // If email exists
+                // get the hashed password from the database
+                // Match the hashed password with the one which user entered
+                // if it does not match. -> error
+                $hashed = $user->getPasswordhashed();
+                $status = password_verify($password, $hashed);
+
+
+                // check if the plain text password is valid
+                if ($status) {
+                    $_SESSION['email'] = $email;
+
+                    $username = $dao->getFullname();
+                    $_SESSION['username'] = $username;
+                    exit;
+                } else {
+                    // password not valid
+                    // return to login page and show error
+                    $errors['password'][] = 'Invalid password.';
+                }
+            } else {
+                $errors['email'][] = 'This email account does not exist.';
+            }
+            if (count($errors) > 0) {
+                // header('Location: login.php');
+                echo json_encode($errors);
+                exit();
+            }
+
+            // debug
+            // echo json_encode($_SESSION);
+            ?>
+
+
+        }
     </script>
     <script src="validation_page.js"></script>
 
 </body>
+
 </html>
