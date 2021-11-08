01a488f8b55bc26e46ce6493ed73da6acec049b1
LIM-YU-XUAN <76712654+LIM-YU-XUAN@users.noreply.github.com>
2021-11-09 07:51:40 +0800

    forum page updates
    - made changes to the navigation bar (padding)
    - linked some of the completed pages to the nav bar (signup, login, forum, home)
    - changed css of forum display
    
    * filter is still not working

--- forum_dynamic/display.php
+++ forum_dynamic/display.php
@@ -21,10 +21,8 @@ $posts = array_reverse($posts); // Get an Indexed Array of Post objects
     <head>
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-
         <!-- Axios -->
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-
         <!-- Vue3 CDN -->
         <script src="https://unpkg.com/vue@next"></script>
 
@@ -32,11 +30,15 @@ $posts = array_reverse($posts); // Get an Indexed Array of Post objects
             .text {
                 font-family: Georgia, 'Times New Roman', Times, serif;
             }
-
-            div.sticky {
-                position: sticky;
-                top: 0;
+            .somePosition {
+                /* position: fixed; */
+                /* bottom: 50; */
             }
+            .card-background {
+                back
+            }
+
+            
         </style>
 
         <link rel="stylesheet" href="navbar.css">
@@ -45,126 +47,123 @@ $posts = array_reverse($posts); // Get an Indexed Array of Post objects
     </head>
 <body>
 
+            <!-- navigation bar -->
+    <nav>
+        <div class="nav-logo">
+            <a href="#" id="nav-logo">next stop.</a>
+        </div>
 
-    <div class='row'>
-        <nav>
-            <div class="nav-logo">
-                <a href="#" id="nav-logo">next stop.</a>
-            </div>  
+        <div class="nav-items" id="nav-items">
+            <a href="../homepage/homepage.php" class="nav-item" id="nav-home">Home</a>
+            <a href="display.php" class="nav-item" id="nav-forum">Forum</a>
+            <a href="#" class="nav-item" id="nav-uniGuide">University Guide</a>
+            <a href="#" class="nav-item" id="nav-login-signup">Login/Sign Up</a>
+        </div>
 
-            <div class="nav-items" id="nav-items">
-                <a href="#" class="nav-item" id="nav-home">Home</a>
-                <a href="#" class="nav-item" id="nav-forum">Forum</a>
-                <a href="#" class="nav-item" id="nav-uniGuide">University Guide</a>
-                <a href="#" class="nav-item" id="nav-login-signup">Login/Sign Up</a>
-            </div>
+        <div class="nav-login-signup">
+            <a href="./log_in.html" class="nav-login" id="nav-login">Login</a>
+            <a href="./sign_up.html" class="nav-signup" id="nav-signup">Sign Up</a>
+        </div>
 
-            <div class="nav-login-signup">
-                <a href="#" class="nav-login" id="nav-login">Login</a>
-                <a href="#" class="nav-signup" id="nav-signup">Sign Up</a>
-            </div>
+        <div class="hamburger-menu" id="hamburger-menu" onclick="toggleMenu()">
+            <span class="menu menu-small menu-top"></span>
+            <span class="menu menu-middle"></span>
+            <span class="menu menu-small menu-bottom"></span>
+        </div>
+    </nav>
 
-            <div class="hamburger-menu" id="hamburger-menu" onclick="toggleMenu()">
-                <span class="menu menu-small menu-top"></span>
-                <span class="menu menu-middle"></span>
-                <span class="menu menu-small menu-bottom"></span>
-            </div>
-        </nav>
-
-        <script>
-            var navItems = document.getElementById("nav-items");
-            navItems.style.maxHeight = "0px";
-            function toggleMenu(){
-                if( navItems.style.maxHeight == "0px" ){
-                    navItems.style.maxHeight = "200px";
-                } else {
-                    navItems.style.maxHeight = "0px";
-                }
+    <script>
+        var navItems = document.getElementById("nav-items");
+        navItems.style.maxHeight = "0px";
+        function toggleMenu() {
+            if (navItems.style.maxHeight == "0px") {
+                navItems.style.maxHeight = "200px";
+            } else {
+                navItems.style.maxHeight = "0px";
             }
-        </script>
-    </div>
+        }
+    </script>
+
+    <!-- end of navigation bar -->
 
     <div class='container'>
-        <div class='row header'>
-            <h1> Forum </h1>
-        </div>
 
-        <div class="row sticky-top" style="background-color: white;">
-            <a href='add.html' class='btn btn-warning'><h3> Share Your Experience! </h3></a>
+        <div class="row">
 
-            <div id="app" class="text-center">
+            <div class="col-4" style="margin-top: 10%;">
+                <div class="sticky-xl-top text-center">
+                    <div style="background-color: #ff93ae; padding: 25%; border-radius: 25px;">
 
-            <br>
+                        <a href='add.html' class='btn btn-warning'><h3> Share Your Experience! </h3></a>
 
-            <h4> Filter Your Search </h4>
+                        <div id="app" class="text-center">
 
-                <div class="row text-center">
-                    <label for='country'> Choose a Country: </label>
-                        <select name='country' id='country'>
-                            <option value='*'> All </option>
-                            <option value="Korea"> Korea </option>
-                            <option v-for="country in countArr" :value="country"> {{country}} </option>
-                        </select>
-                    </select>
+                            <br>
+                            <!-- search filter -->
+                            <h4> Filter Your Search </h4>
+
+                            <div class="row text-center">
+                                <label for='country'> Choose a Country: </label>
+                                    <select name='country' id='country'>
+                                        <option value='*'> All </option>
+                                        <!-- <option value="Korea"> Korea </option> -->
+                                        <option v-for="country in countArr" :value="country"> {{country}} </option>
+                                    </select>
+                                </select>
+                            </div>
+
+                            <b> or </b>
+
+                            <div class="row text-center">
+                                <label for='uni'> Choose a University: </label>
+                                    <select name='uni' id='uni'>
+                                        <option value='*'> All </option>
+                                        <!-- <option value="SNU"> SNU </option> -->
+                                        <option v-for="uni in uniArr" :value="uni"> {{uni}} </option>
+                                    </select>
+                                </select>
+                            </div>
+                            <!-- end of search filter -->
+
+                            <br>
+                            <input class="btn btn-success" type='submit' value='Go!'/>
+                            <br>
+
+                        </div>
+
+                    </div>
                 </div>
-
-                <b> or </b>
-
-                <div class="row text-center">
-                    <label for='uni'> Choose a University: </label>
-                        <select name='uni' id='uni'>
-                            <option value='*'> All </option>
-                            <option value="SNU"> SNU </option>
-                            <option v-for="uni in uniArr" :value="uni"> {{uni}} </option>
-                        </select>
-                    </select>
-                </div>
-
-
-                <br>
-                <input class="btn btn-success" type='submit' value='Go!'/>
-                <br>
-
             </div>
 
-        </div>
-
-        
-
-        <!-- <div class="row"> -->
-        <br>
-        <div class='row'>
-
-            <?php
-                if( count($posts) > 0 ) {
-
-                    foreach($posts as $post_object ) {
-                        echo "
-
-                        <div class='card mb-3 border-warning'>
-                            <div class='card-body'>
-                                <h5 class='card-title'>{$post_object->getSubject()}</h5>
-                                <h6 class='card-subtitle mb-2 text-muted' style='display: none;'> Last Updated: {$post_object->getUpdateTimestamp()}</h6>
-                                <h6 class='card-subtitle mb-2 text-muted'> Country: {$post_object->getCountry()}</h6>
-                                <h6 class='card-subtitle mb-2 text-muted'> University: {$post_object->getUniversity()}</h6>
-                                <p class='card-text'>{$post_object->getEntry()}</p>
-                                <a style='display: none; href='edit.php?id={$post_object->getID()}'>Edit</a>
-                                <a style='display: none;' href='delete.php?id={$post_object->getID()}'>Delete</a>
-                                <p class='card-text'><small class='text-muted'>Last updated on {$post_object->getUpdateTimestamp()} by <b>@{$post_object->getUsername()}</b> </small></p>
+            <br>
+            <div class='col-8'>
+                <?php
+                    if( count($posts) > 0 ) {
+                        foreach($posts as $post_object ) {
+                            echo "
+                            <div class='card mb-3 border-warning'>
+                                <div class='card-body' style='background-color: #fdd0b6;'>
+                                    <h5 class='card-title'>{$post_object->getSubject()}</h5>
+                                    <h6 class='card-subtitle mb-2 text-muted' style='display: none;'> Last Updated: {$post_object->getUpdateTimestamp()}</h6>
+                                    <h6 class='card-subtitle mb-2 text-muted'> Country: {$post_object->getCountry()}</h6>
+                                    <h6 class='card-subtitle mb-2 text-muted'> University: {$post_object->getUniversity()}</h6>
+                                    <p class='card-text'>{$post_object->getEntry()}</p>
+                                    <a style='display: none;' href='edit.php?id={$post_object->getID()}'>Edit</a>
+                                    <a style='display: none;' href='delete.php?id={$post_object->getID()}'>Delete</a>
+                                    <p class='card-text'><small class='text-muted'>Last updated on {$post_object->getUpdateTimestamp()} by <b>@{$post_object->getUsername()}</b> </small></p>
+                                </div>
                             </div>
-                        </div>
-                        ";
+                            ";
+                        }
                     }
-
-                }
-            ?>
+                ?>
+            </div>
 
         </div>
 
     </div>
 
     <script>
-
     const app = Vue.createApp( {
         //=========== DATA PROPERTIES ===========
         data() {
