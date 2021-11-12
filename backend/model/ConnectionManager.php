e27ed328bebc9d4eecb2e1120fef47884f9be164
LIM-YU-XUAN <76712654+LIM-YU-XUAN@users.noreply.github.com>
2021-11-12 21:11:59 +0800

    heroku

--- backend/model/ConnectionManager.php
+++ backend/model/ConnectionManager.php
@@ -15,18 +15,30 @@ class ConnectionManager {
                 
         // // if fail, exception will be thrown
 
-        // Get Heroku ClearDB connection information
-        $cleardb_url = parse_url(getenv("mysql://b6bd77d7a28d3b:f1bdda31@us-cdbr-east-04.cleardb.com/heroku_38ec8b215cf2e16?reconnect=true"));
-        $cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
-        $cleardb_username = $cleardb_url["b6bd77d7a28d3b"];
-        $cleardb_password = $cleardb_url["f1bdda31"];
-        $cleardb_db = substr($cleardb_url["heroku_38ec8b215cf2e16"],1);
-        $active_group = 'default';
-        $query_builder = TRUE;
-        // Connect to DB
-        $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+                            // // Get Heroku ClearDB connection information
+                            // $cleardb_url = parse_url(getenv("mysql://b6bd77d7a28d3b:f1bdda31@us-cdbr-east-04.cleardb.com/heroku_38ec8b215cf2e16?reconnect=true"));
+                            // $cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
+                            // $cleardb_username = $cleardb_url["b6bd77d7a28d3b"];
+                            // $cleardb_password = $cleardb_url["f1bdda31"];
+                            // $cleardb_db = substr($cleardb_url["heroku_38ec8b215cf2e16"],1);
+                            // $active_group = 'default';
+                            // $query_builder = TRUE;
+                            // // Connect to DB
+                            // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-        return $conn;
+        
+                            $servername = 'us-cdbr-east-04.cleardb.com';
+                            $username = 'b6bd77d7a28d3b';
+                            // $password = 'root'; // -> MAMP server
+                            $password = 'f1bdda31'; // -> WAMP server
+                            $dbname = 'heroku_38ec8b215cf2e16';
+                            
+                            // Create connection
+                            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);     
+                            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // if fail, exception will be thrown
+                    
+                            // Return connection object
+                            return $conn;
 
     }
     
