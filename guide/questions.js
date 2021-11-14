75da57195008582dce103f1655060d82d9173252
LIM-YU-XUAN <76712654+LIM-YU-XUAN@users.noreply.github.com>
2021-11-14 19:58:48 +0800

    central asia and africa questions

--- guide/questions.js
+++ guide/questions.js
@@ -267,106 +267,106 @@ var middleEast = [
 
 var centralAsia = [
     {
-        question: "1. Which is the largest religious monument in the World?",
+        question: "1. Which country is NOT in Central Asia",
         correct: 3,
         answers: [
-            { option: "Borobudur - Central Java, Indonesia", answer: false },
-            { option: "Spring Temple Buddha - Henan, China", answer: false },
-            { option: "Karnak Temple Complex - Luxor, Egypt", answer: false },
-            { option: "Angkor Wat - Siem Reap, Cambodia", answer: true }
+            { option: "Kazakhstan", answer: false },
+            { option: "Kyrgyz Republic", answer: false },
+            { option: "Turkmenistan", answer: false },
+            { option: "Cambodia", answer: true }
         ]
     },
     {
-        question: "2. How many rooms does The Forbidden City in Beijing, China, contains?",
+        question: "2. Plov is the national dish of both Tajikistan and Uzbekistan. What does it consist of?",
         correct: 3,
         answers: [
-            { option: "20", answer: false },
-            { option: "50", answer: false },
-            { option: "600", answer: false },
-            { option: "9000", answer: true }
+            { option: "It is a rice dish with preserved vegetables", answer: false },
+            { option: "It is a noodle dish with meat and an assortment of vegetables", answer: false },
+            { option: "It is a vegetarian dish", answer: false },
+            { option: "It's a rice dish with meat, grated carrots and onions", answer: true }
         ]
     },
     {
-        question: "3. Which country still has a reigning emperor?",
+        question: "3. Which of the following is the capital of Kazakhstan?",
         correct: 1,
         answers: [
-            { option: "Thailand", answer: false },
-            { option: "Japan", answer: true },
-            { option: "United Kingdom", answer: false },
-            { option: "None left", answer: false }
+            { option: "Bishkek", answer: false },
+            { option: "Nur-Sultan", answer: true },
+            { option: "Ashgabat", answer: false },
+            { option: "Tashkent", answer: false }
         ]
     },
     {
-        question: "4. Which kind of park does not exist in South Korea?",
+        question: "4. Why is the Turkmen gas crater, Darvaza, known as the ‘Gate of Hell’?",
         correct: 0,
         answers: [
-            { option: "Elephant-themed park", answer: true },
-            { option: "Toilet-themed park", answer: false },
-            { option: "Sex-themed park", answer: false },
-            { option: "Penis-themed park", answer: false }
+            { option: "The Darvaza crater is still burning, since 1971", answer: true },
+            { option: "The Darvaza crater is active and erupts every now and then", answer: false },
+            { option: "The Darvaza crater is red and dark", answer: false },
+            { option: "The Darvaza crater was once volcanic but is not active anymore", answer: false }
         ]
     },
     {
-        question: "5. What is the Vietnamese currency?",
+        question: "5. Which of the following is a famous cuisine in Central Asia",
         correct: 1,
         answers: [
-            { option: "Rupee", answer: false },
-            { option: "Dong", answer: true },
-            { option: "Peso", answer: false },
-            { option: "Lao kip", answer: false }
+            { option: "Taco", answer: false },
+            { option: "Manti Dumpling", answer: true },
+            { option: "Sushi", answer: false },
+            { option: "Pho", answer: false }
         ]
     }
 ]
 
 var africa = [
     {
-        question: "1. Which is the largest religious monument in the World?",
-        correct: 3,
+        question: "1. How many countries are there in Africa today?",
+        correct: 2,
         answers: [
-            { option: "Borobudur - Central Java, Indonesia", answer: false },
-            { option: "Spring Temple Buddha - Henan, China", answer: false },
-            { option: "Karnak Temple Complex - Luxor, Egypt", answer: false },
-            { option: "Angkor Wat - Siem Reap, Cambodia", answer: true }
+            { option: "12", answer: false },
+            { option: "32", answer: false },
+            { option: "54", answer: true },
+            { option: "78", answer: false }
         ]
     },
     {
-        question: "2. How many rooms does The Forbidden City in Beijing, China, contains?",
-        correct: 3,
-        answers: [
-            { option: "20", answer: false },
-            { option: "50", answer: false },
-            { option: "600", answer: false },
-            { option: "9000", answer: true }
-        ]
-    },
-    {
-        question: "3. Which country still has a reigning emperor?",
+        question: "2. Which is the largest country in mainland Africa?",
         correct: 1,
         answers: [
-            { option: "Thailand", answer: false },
-            { option: "Japan", answer: true },
-            { option: "United Kingdom", answer: false },
-            { option: "None left", answer: false }
+            { option: "Gambia", answer: false },
+            { option: "Algeria", answer: true },
+            { option: "Angola", answer: false },
+            { option: "Libya", answer: false }
         ]
     },
     {
-        question: "4. Which kind of park does not exist in South Korea?",
+        question: "3. Which African country was the only known home of the now extinct dodo?",
         correct: 0,
         answers: [
-            { option: "Elephant-themed park", answer: true },
-            { option: "Toilet-themed park", answer: false },
-            { option: "Sex-themed park", answer: false },
-            { option: "Penis-themed park", answer: false }
+            { option: "Mauritius", answer: true },
+            { option: "Nigeria", answer: false },
+            { option: "Madagascar", answer: false },
+            { option: "Guinea", answer: false }
         ]
     },
     {
-        question: "5. What is the Vietnamese currency?",
-        correct: 1,
+        question: "4. Which is the tallest mountain in Africa",
+        correct: 2,
         answers: [
-            { option: "Rupee", answer: false },
-            { option: "Dong", answer: true },
-            { option: "Peso", answer: false },
-            { option: "Lao kip", answer: false }
+            { option: "Mount Kenya, Kenya", answer: false },
+            { option: "Mount Speke, Uganda", answer: false },
+            { option: "Mount Kilimanjaro, Tanzania", answer: true },
+            { option: "Mount Emin, Democratic Republic of Congo", answer: false }
+        ]
+    },
+    {
+        question: "5. Out of 100%, what percentage of the world’s total land area does Africa make up?",
+        correct: 2,
+        answers: [
+            { option: "5%", answer: false },
+            { option: "10%", answer: false },
+            { option: "20%", answer: true },
+            { option: "35%", answer: false }
         ]
     }
 ]
